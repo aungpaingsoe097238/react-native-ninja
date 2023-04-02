13eac9a207063c821938ffcf66ef452efbda33c9
Aung Paing Soe <aungpaingsoe097238@gmail.com>
2023-04-02 21:58:55 +0630

    alert

--- App.js
+++ App.js
@@ -1,32 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
-import { FlatList, StyleSheet, Text, View } from 'react-native';
-import Header from './components/Header';
-import ToDoItem from './components/ToDoItem';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { Alert, FlatList, StyleSheet, Text, View } from "react-native";
+import Header from "./components/Header";
+import ToDoItem from "./components/ToDoItem";
+import AddToDo from "./components/AddToDo";
 
 export default function App() {
-
-  const [lists,setLists] = useState([
-    { 'name' : 'apple', 'key' : 1 },
-    { 'name' : 'banana', 'key' : 2 },
-    { 'name' : 'orange', 'key' : 3 }
+  const [lists, setLists] = useState([
+    { name: "apple", key: 1 },
+    { name: "banana", key: 2 },
+    { name: "orange", key: 3 },
   ]);
 
   const pressHandler = (key) => {
     setLists((lists) => {
-      return lists.filter((item)=> item.key !== key )
-    })
+      return lists.filter((item) => item.key !== key);
+    });
+  };
+
+  const submitHandler = (todo) => {
+    if(todo.length > 3){
+      setLists((lists) => {
+        return [
+          { 'name' : todo, 'key' : Math.random().toString() },
+          ...lists
+        ];
+      })
+    }else{
+      Alert.alert('OOH!','Todo must be over 3 char long.',[
+        {
+          text : 'Ok', onPress: () => console.log('Alert Closed.')
+        }
+      ]);
+    }
+
   }
 
   return (
     <View style={styles.container}>
-      <Header/>
-      <View style={styles.context}> 
+      <Header />
+      <View style={styles.context}>
         <View style={styles.list}>
+          <AddToDo submitHandler={submitHandler} />
           <FlatList
-            data={ lists }
-            renderItem={ ({ item }) => <ToDoItem item={item} pressHandler={pressHandler} /> } 
-            keyExtractor={ (item) => item.key }
+            data={lists}
+            renderItem={({ item }) => (
+              <ToDoItem item={item} pressHandler={pressHandler}  />
+            )}
+            keyExtractor={(item) => item.key}
           />
         </View>
       </View>
@@ -37,9 +58,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
   },
-  context : {
-    padding : 30
-  }
+  context: {
+    padding: 30,
+  },
 });
--- components/Header.js
+++ components/Header.js
@@ -12,7 +12,7 @@ const styles = StyleSheet.create({
         display : 'flex',
         justifyContent : 'center',
         alignItems: 'center',  
-        backgroundColor : "green",
+        backgroundColor : "coral",
         color: "#fff"
     },
     title : {
