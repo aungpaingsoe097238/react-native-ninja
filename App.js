8d48387b93c99a2373435cfc755a556fb4aa2f0c
AungPaingSoe <aungpaingsoe097238@gmail.com>
2023-04-03 11:17:23 +0630

    change

--- App.js
+++ App.js
@@ -1,10 +1,8 @@
-import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
 import {
   Alert,
   FlatList,
   StyleSheet,
-  Text,
   View,
   TouchableWithoutFeedback,
   Keyboard,
